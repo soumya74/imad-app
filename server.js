118f3ecd725153915438d44407eda61e40301851
SSG <30518422+soumya74@users.noreply.github.com>
2017-08-06 19:20:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,14 +73,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
