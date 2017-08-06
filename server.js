20f6c914acb8490bb38e875fde4b512bf3225a94
SSG <30518422+soumya74@users.noreply.github.com>
2017-08-06 17:45:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'))
-})
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
