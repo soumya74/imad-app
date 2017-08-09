94f097a03c0b7b13fa60f18d603c683cb02e2261
SSG <30518422+soumya74@users.noreply.github.com>
2017-08-10 01:04:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+var list='';
+var nameList = [];
+var nameInput = document.GetElementbyId("name");
+var submit_btn = document.GetElementbyId("submit_btn");
+var ul = document.GetElementbyId("ulNameList");
+
+submit_btn.onclick = function(){
+    var nameValue = nameInput.value;
+    nameList.push(nameValue);
+    
+    for(var i=0;i<nameList.length; i++)
+    {
+        list+="<li>"+nameList[i]+"</li>";
+    }
+    ul.innerHTML(list);
+};
+
+
