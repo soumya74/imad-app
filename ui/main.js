5b034698a11db84c1a38166476e324f41c9e025d
SSG <30518422+soumya74@users.noreply.github.com>
2017-08-10 01:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,15 +5,28 @@ var nameList = [];
 
 submit_btn.onclick = function(){
     var list='';
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange() = function(){
+      if(request.readyState===XMLHttp.DONE)
+      {
+          if(request.status===200)
+          {
+              var names = request.responseText;
+              names= JSON.parsify(names);
+              for(var i=0;i<names.length; i++)
+              {
+                  list+="<li>"+nameList[i]+"</li>";
+              }
+              ul.innerHTML = list;
+          }
+      }
+    }
+    
     var nameInput = document.getElementById("name");
     var nameValue = nameInput.value;
-    nameList.push(nameValue);
-    
-    for(var i=0;i<nameList.length; i++)
-    {
-        list+="<li>"+nameList[i]+"</li>";
-    }
-    ul.innerHTML = list;
+    request.open('GET','http://soumya74.imad.hasura-app.io/submit-name?name=' + nameValue);
+    request.send(null);
 };
 
 
