63916ce07924654406730a58788c43c4836989af
SSG <30518422+soumya74@users.noreply.github.com>
2017-08-11 16:59:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,18 +8,15 @@ submit_btn.onclick = function(){
     var request = new XMLHttpRequest();
 
     request.onreadystatechange() = function(){
-      if(request.readyState===XMLHttp.DONE)
+      if(request.readyState===XMLHttp.DONE && request.status===200)
       {
-          if(request.status===200)
+          var names = request.responseText;
+          names= JSON.parsify(names);
+          for(var i=0;i<names.length; i++)
           {
-              var names = request.responseText;
-              names= JSON.parsify(names);
-              for(var i=0;i<names.length; i++)
-              {
-                  list+="<li>"+nameList[i]+"</li>";
-              }
-              ul.innerHTML = list;
+              list+="<li>"+nameList[i]+"</li>";
           }
+          ul.innerHTML = list;
       }
     };
     
